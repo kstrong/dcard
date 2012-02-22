d59d0c91885205a1bf2775a2590dff12023258d7
kstrong <kevin@wordsauce.org>
2012-02-21 20:25:14 -0800

    tabs to spaces

--- dc.rb
+++ dc.rb
@@ -8,38 +8,38 @@ enable :sessions
 DataMapper.setup :default, settings.db
 
 class Code
-	include DataMapper::Resource
+    include DataMapper::Resource
 
-	property :id,         Serial
-	property :code,       String
-	property :created_at, DateTime
+    property :id,         Serial
+    property :code,       String
+    property :created_at, DateTime
 
-	has n, :downloads, :through => Resource
+    has n, :downloads, :through => Resource
 end
 
 class Download
-	include DataMapper::Resource
+    include DataMapper::Resource
 
-	property :id,      Serial
-	property :title,   String
-	property :path,    String
-	property :format,  String
-	property :storage, String
+    property :id,      Serial
+    property :title,   String
+    property :path,    String
+    property :format,  String
+    property :storage, String
 
-	has n, :codes, :through => Resource
-	has n, :download_logs
+    has n, :codes, :through => Resource
+    has n, :download_logs
 end
 
 class DownloadLog
-	include DataMapper::Resource
+    include DataMapper::Resource
 
-	belongs_to :download, :key => true
+    belongs_to :download, :key => true
 
-	property :code,       String
-	property :ip_address, String
-	property :email,      String
-	property :format,     String
-	property :timestamp,  DateTime
+    property :code,       String
+    property :ip_address, String
+    property :email,      String
+    property :format,     String
+    property :timestamp,  DateTime
 end
 
 DataMapper.finalize
@@ -49,69 +49,69 @@ DataMapper.auto_upgrade!
 codes = ['secretsauce']
 
 get '/' do
-	'welcome'
+    'welcome'
 end
 
 get '/download' do
-	erb :access
+    erb :access
 end
 
 post '/download' do
-	if ! params.has_key? 'code' 
-		flash[:error] = "Please enter a download code."
-		redirect to('/download')
-	end
+    if ! params.has_key? 'code' 
+        flash[:error] = "Please enter a download code."
+        redirect to('/download')
+    end
 
-	if codes.include? params[:code] 
-		session[:code] = params[:code]
-		if params[:email] 
-			session[:email] = params[:email]
-		end
-		redirect to("/download/#{params['code']}")
-	else
-		flash[:error] = "This code is invalid."
-		redirect to('/download')
-	end
+    if codes.include? params[:code] 
+        session[:code] = params[:code]
+        if params[:email] 
+            session[:email] = params[:email]
+        end
+        redirect to("/download/#{params['code']}")
+    else
+        flash[:error] = "This code is invalid."
+        redirect to('/download')
+    end
 end
 
 get '/download/:code' do
-	redirect '/download' unless codes.include? params[:code]
-	@downloads = Code.first(:code => params[:code]).downloads
-	erb :download
+    redirect '/download' unless codes.include? params[:code]
+    @downloads = Code.first(:code => params[:code]).downloads
+    erb :download
 end
 
 get '/download/:code/:download_id' do
-	if ! codes.include? params[:code] 
-		halt 404
-	end
+    if ! codes.include? params[:code] 
+        halt 404
+    end
 
-	download = Download.first(:id => params[:download_id], 
-	                          :codes => { :code => params[:code] })
-	
-	puts download.inspect
-	if ! download
-		halt 403
-	end
+    download = Download.first(:id => params[:download_id], 
+                              :codes => { :code => params[:code] })
+    
+    puts download.inspect
+    if ! download
+        halt 403
+    end
 
-	if download.storage == "local"
-		DownloadLog.create(
-		  :code => params[:code],
-		  :download => download,
-		  :ip_address => request.ip,
-		  :email => session[:email],
-		  :format => download.format,
-		  :timestamp => Time.now
-		)
-		send_file File.join(Dir.pwd, download.path)
-	else
-		halt 501
-	end
+    if download.storage == "local"
+        DownloadLog.create(
+          :code => params[:code],
+          :download => download,
+          :ip_address => request.ip,
+          :email => session[:email],
+          :format => download.format,
+          :timestamp => Time.now
+        )
+        send_file File.join(Dir.pwd, download.path)
+    else
+        halt 501
+    end
 end
 
 get '/manage' do
-	erb :admin
+    erb :admin
 end
 
 not_found do
-	halt 404, 'not found'
+    halt 404, 'not found'
 end
